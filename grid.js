18e74c2f77c6abfbbb506c8a47b109885c07ed9a
Ben Townshend <bentownshend96@gmail.com>
2016-08-19 20:19:50 +0100

    Grid System

---
+++ grid.js
@@ -0,0 +1,75 @@
+function fadeIn (el, multiplier) {
+    el.style.opacity = 0;
+
+    var last = +new Date();
+	var tick = function() {
+        el.style.opacity = +el.style.opacity + (new Date() - last) / multiplier;
+        last = +new Date();
+
+        if (+el.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        } else {
+            el.style.opacity = 1;
+        }
+    }
+    tick();
+}
+
+function fadeOut (el, multiplier) {
+    el.style.opacity = 1;
+
+    var last = +new Date();
+	var tick = function() {
+        el.style.opacity = +el.style.opacity - (new Date() - last) / multiplier;
+        last = +new Date();
+
+        if (+el.style.opacity > 0) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick) || setTimeout(tick, 16));
+        } else {
+            el.style.opacity = 0;
+        }
+    }
+    tick();
+}
+
+function isLandscape() {
+	if (window.innerWidth >= window.innerHeight) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function sizeGrid(columns) {
+	var items = document.getElementsByClassName('item'),
+		grid = document.getElementById('grid');
+
+	if (items !== null && grid !== null) {
+		var itemSize = (grid.offsetWidth / columns) + 'px';
+
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i];
+
+			item.style.width = itemSize;
+			item.style.height = itemSize;
+		}
+	}
+}
+
+function gridItemMouseOver(el) {
+	fadeIn(el.getElementsByClassName('itemContent')[0], 200);
+}
+
+function gridItemMouseOut(el) {
+	fadeOut(el.getElementsByClassName('itemContent')[0], 200);
+}
+
+var gridColumns = function() {
+	if (isLandscape()) {
+		sizeGrid(3);
+	} else {
+		sizeGrid(1);
+	}
+}
+
+window.onresize(gridColumns);
